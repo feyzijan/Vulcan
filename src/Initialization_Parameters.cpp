5799c3369b90d81b135bce4a1ef987ad86484cee
Salvador <s.navarro@imperial.ac.uk>
2022-12-07 11:54:39 +0000

    makefile changes

--- src/Initialization_Parameters.cpp
+++ src/Initialization_Parameters.cpp
@@ -7,7 +7,7 @@ namespace constants
 {
     // since the actual variables are inside a namespace, the forward declarations need to be inside a namespace as well
     //extern const int job_market_size {100};
-    extern const int n_households {100};
-    extern const int n_consumer_firms {10};
-    extern const int n_capital_firms{5};
+    extern const int n_households {1000000};
+    extern const int n_consumer_firms {100000};
+    extern const int n_capital_firms{5000};
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,56 @@ using namespace std;
 
 int main()
 {
-
-
+//  -------- STEP 0 INITIALIZATION -------------------
+    
+    // STEP 0.1: Initilaize market size, households, etc..
     cout << job_market_size << endl;
 
+    // STEP 0.2: Initialize the Public Board
+        //Public_Info_Board* pPublic_Info_Board = new Public_Info_Board();
 
-    //Public_Info_Board* pPublic_Info_Board = new Public_Info_Board();
+    // STEP 0.3: Initialize the Bank and Government
 
+    // STEP 0.4: Allocate and Initialize Firms
+
+    // STEP 0.5: Allocate and Initialize Jobs and Job Market 
+    Job* job_1 = new Job(nullptr,10,1);
+    job_1->Print();
+
+
+    // STEP 0.6: Allocate and Initialize Households
+
+        // saving = distribution_gauss
+        // saving = distribution_data
+        // saving = random(0,10)
+    
     Household_Agent* Household_1 = new Household_Agent(1000, 10, false, false, 500, 0.4,0.2);
 
     Household_1->Print();
 
+//  -------- STEP 1 MAIN LOOP -------------------
 
-    Job* job_1 = new Job(nullptr,10,1);
-    job_1->Print();
+    // STEP 1.1: Update Households , Firm
+    // Households(n+1/2) =f [InfoBoard(n)] 
+    // Firms(n+1/2)      = f[InfoBoard(n)] 
+
+    // STEP 1.2: Add Jobs/Goods/Capital from Firms  (Job/interaction)      
+    // JobMarket(n+1/2)  = f(Firms(n+1/2))
+    // Sort the job
+    // GoodMarket ...
+    
+    // STEP 1.3: Update Household  based on Markets
+    // Households(n+1) = f(JobMarket(n+1/2), Household(n+1/2))
+    // JobMarket(n+1)  = f(Firms(n+1))
+    
+
+    // Update InfoBoard (done automatically)
+    // InfoBoard(n+1) = JobMarket(n+1),CapitalGoodMarket(n+1),CapitalGoodMarket(n+1)
+
+
+//  -------- STEP 1 MAIN LOOP -------------------
+
+
 
 
 
